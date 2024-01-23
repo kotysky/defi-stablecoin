99d5d56b078d66e8cc2fb88d6cb15c4673f49146
JJ Huertas <kotysky@Belcebu>
2024-01-23 19:40:31 +0000

    hey

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DCSEngine} from "../src/DSCEngine.sol";
+
+contract DeployDSC is Script {
+    function run() external returns (DecentralizedStableCoin, DCSEngine) {
+        vm.startBroadcast();
+
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+
+        //DSCEngine engine = new DCSEngine()
+
+        vm.stopBroadcast();
+    }
+}
